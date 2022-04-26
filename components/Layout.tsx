d7cb7dc143df07be0dc0d5312c2ec12b8f130d3d
Compey <hi@devcomp.tk>
2022-04-26 21:19:27 +0530

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,19 +9,19 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="overflow-x-hidden relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>flightpkg | home</title>
+      <title>DevComp | home</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
         content="Swift, reliable, multi-language package manager."
       />
-      <meta property="og:title" content={`flightpkg | home`} />
+      <meta property="og:title" content={`DevComp | home`} />
       <meta
         property="og:description"
-        content="Swift, reliable, multi-language package manager."
+        content="Hi, I'm DevComp, a JavaScript developer."
       />
-      <meta property="og:url" content="https://flightpkg.js.org" />
+      <meta property="og:url" content="https://devcomp.xyz" />
       <meta property="og:type" content="website" />
     </Head>
     {children}
